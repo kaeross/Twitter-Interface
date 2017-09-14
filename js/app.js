827bc9d963772d04d50ad8aa7a0197239aa9fc23
Kate Ross <kaeross@icloud.com>
2017-09-14 04:49:45 +0100

    Initial commit - convert html to pug

--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
-app.use();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const router = express.Router();
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.route('/', () => {
+app.use(bodyParser.urlencoded({'extended': false}));
+app.use(cookieParser());
+app.use('/static', express.static('public'));
 
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+	const username = '@kateross01';
+	res.render('index',  { username }  );
 })
 
-listen(3000);
+// app.use((req, res, next) => {
+// 	const err = new Error('Not Found');
+
+// 	err.status = 404;
+// 	next(err);
+// });
+
+// app.use((err, req, res) => {
+// 	res.locals.error = err;
+// 	res.status(err.status);
+// 	res.render('error');
+// });
+
+app.listen(port, () => {
+	console.log(`The application is running on ${port}`);
+});
