9ab09c04ba8a18215264a59450766e5880e4c1dc
Kate Ross <kaeross@icloud.com>
2017-10-19 19:32:20 +0100

    Add direct messaging functions

--- js/routes/index.js
+++ js/routes/index.js
@@ -15,9 +15,10 @@ const moment = require('moment');
 
 const T = new Twit(oAuth);
 const ifData = {};
-    ifData.userInfo = {};
-    ifData.tweets = [];
-    ifData.friendInfo = [];
+ifData.userInfo = {};
+ifData.tweets = [];
+ifData.friendInfo = [];
+ifData.conversations = [];
 
 /****************************************************************
  * General functions
@@ -110,14 +111,18 @@ const getUserInfo = (req, res, next) => {
  * Timeline functions
  ***************************************************************/
 
-const getTimelineData = (res, req, next) => {
+const getTimelineData = (req, res, next) => {
     T.get('https://api.twitter.com/1.1/statuses/home_timeline.json?count=5&exclude_replies')
         .catch(function (err) {
             console.log('Could not retrieve friend data', err.stack);
             next(err);
         })
-        .then(function(response) {
+        .then(function (response) {
             const timelineData = response.data;
+            if (timelineData.errors) {
+                var error = new Error(timelineData.errors.message);
+                console.error(error);
+            }
             for (let i = 0; i < timelineData.length; i += 1) {
                 const tweet = {
                     name: timelineData[i].user.name,
@@ -139,14 +144,18 @@ const getTimelineData = (res, req, next) => {
  ***************************************************************/
 
 //get 5 items from following / friends list from twitter api
-const getFriendsData = (res, req, next) => {
+const getFriendsData = (req, res, next) => {
     T.get('https://api.twitter.com/1.1/friends/list.json?count=5')
         .catch(function (err) {
-            console.log('There was a problem retrieving friend data', err.stack);
+            q.log('There was a problem retrieving friend data', err.stack);
             next(err);
         })
         .then(function (response) {
             const friendData = response.data.users;
+            if (friendData.errors) {
+                var error = new Error(friendData.errors.message);
+                console.error(error);
+            }
             for (let i = 0; i < friendData.length; i += 1) {
                 const friend = {
                     name: friendData[i].name,
@@ -160,10 +169,137 @@ const getFriendsData = (res, req, next) => {
     setTimeout(next, 1000);
 };
 
-router.use(getUserInfo, getTimelineData, getFriendsData);
+/****************************************************************
+ * Direct messages functions
+ ***************************************************************/
+
+//get 5 most recent direct messages RECEIVED from twitter api
+const getDmRecievedData = (req, res, next) => {
+    T.get('https://api.twitter.com/1.1/direct_messages.json?count=5')
+        .catch(function (err) {
+            console.log('There was a problem retrieving recieved DMS', err.stack);
+            next(err);
+        })
+        .then(function (response) {
+            const dmRecievedData = response.data;
+            if (dmRecievedData.errors) {
+                var error = new Error(dmRecievedData.errors.message);
+                console.error(error);
+            }
+            res.locals.DMRInfo = [];
+            //when data has been retrieved store relevant data in DMRInfo object
+            for (let i = 0; i < dmRecievedData.length; i += 1) {
+                res.locals.DMRInfo[i] = {
+                    text: dmRecievedData[i].text,
+                    name: dmRecievedData[i].sender.name,
+                    userName: dmRecievedData[i].sender.screen_name,
+                    recipient: dmRecievedData[i].sender_id,
+                    date: dmRecievedData[i].created_at,
+                    timePosted: parseTwitterDate(dmRecievedData[i].sender.created_at, 'long'),
+                    profilePic: dmRecievedData[i].sender.profile_image_url
+                };
+            }
+        });
+    setTimeout(next, 1000);
+};
+
+//get 5 most recent direct messages SENT from twitter api
+const getDmSentData = (req, res, next) => {
+    T.get('https://api.twitter.com/1.1/direct_messages/sent.json?count=5')
+        .catch(function (err) {
+            console.log('There was a problem retrieving sent DMs', err.stack);
+            next(err);
+        })
+        .then(function (response) {
+            const dmSentData = response.data;
+            if (dmSentData.errors) {
+                var error = new Error(dmSentData.errors.message);
+                console.error(error);
+            }
+            res.locals.DMSInfo = [];
+            for (let i = 0; i < dmSentData.length; i += 1) {
+                res.locals.DMSInfo[i] = {
+                    text: dmSentData[i].text,
+                    name: dmSentData[i].sender.name,
+                    userName: dmSentData[i].sender.screen_name,
+                    id: dmSentData[i].sender_id,
+                    date: dmSentData[i].created_at,
+                    timePosted: parseTwitterDate(dmSentData[i].sender.created_at, 'long'),
+                    profilePic: dmSentData[i].sender.profile_image_url,
+                    recipient: dmSentData[i].recipient.id
+                };
+            }
+        });
+    setTimeout(next, 1000);
+};
+
+const organiseDMs = (req, res, next) => {
+    //Put all direct messages into one array in date / time order
+    //push all items to array
+    let concatDMs = [];
+    const recievedDMs = res.locals.DMRInfo;
+    const sentDMs = res.locals.DMSInfo;
+    // concatDMs = Object.assign(recievedDMs);
+    for (let i = 0; i < recievedDMs.length; i++) {
+        concatDMs.push(recievedDMs[i]);
+    }
+    for (let i = 0; i < sentDMs.length; i++) {
+        concatDMs.push(sentDMs[i]);
+    }
+    //sort into date / time order
+    var byDate = concatDMs.slice(0);
+    byDate.sort(function (a, b) {
+        var x = moment(a.date, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en');
+        var y = moment(b.date, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en');
+        return x < y ? -1 : x > y ? 1 : 0;
+    });
+    //get most recent 5 DMs
+    var leaveFive = byDate.length - 6;
+    for (let i = 0; i <= leaveFive; i++) {
+        byDate.splice(0, 1);
+    }
+    //sort into conversations
+    //create array of single conversation then push to conversations array
+    (function sortCovos() {
+        var arrayIndex = 0;
+        for (let i = 0; i < byDate.length; i += 1) {
+            let conversation = [];
+            //get item in ordered DMs 
+            let matchDM = byDate[i];
+            const compareA = matchDM.recipient;
+            //push dm into new conversation array
+            conversation.push(matchDM);
+            //loop through subsequent dms and if match push to array
+            (function compareDMs() {
+                for (var j = i += 1; j < byDate.length; j += 1) {
+                    var dmB = byDate[j];
+                    var compareB = byDate[j].recipient;
+                    if (compareA === compareB) {
+                        conversation.push(dmB); //add matching DM to array
+                    } else {
+                        ifData.conversations.splice(arrayIndex, 0, conversation); //push array to conversations array
+                        arrayIndex += 1; //next
+                        conversation = []; //clear array
+                        conversation.push(dmB); //start array with non matching DM
+                        i = j; //skip already matched items 
+                        return i;
+                    }
+                }
+            })();
+            //push last conversation array into conversations array
+            ifData.conversations.splice(1, 0, conversation);
+            arrayIndex += 1;
+        } return ifData.conversations;
+    })(); //end sortConvos
+    setTimeout(next, 1000);
+};
+
+
+router.use(getUserInfo, getTimelineData, getFriendsData, getDmRecievedData, getDmSentData, organiseDMs);
 
 router.get('/', (req, res) => {
     console.log(ifData);
+    console.log(ifData.conversations[0][0]);
     res.render('index', { ifData });
 });
 
